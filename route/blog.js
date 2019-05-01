ac076229bc2babc6307ba567fb729445c56662c1
chenyanhongdaima <2078442218@qq.com>
2019-05-01 13:14:49 +0800

    删除没必要的代码二

--- route/blog.js
+++ route/blog.js
@@ -28,7 +28,7 @@ const deleteBlog = {
     path: '/api/blog/delete',
     method: 'post',
     func: (request, response) => {
-        // 浏览器发过来的数据我们一般称之为 form（表单）
+
         let form = request.body
         // 删除数据并返回结果
         let success = blog.delete(form.id)
